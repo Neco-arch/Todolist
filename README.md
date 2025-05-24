8375dc87be86047d12bf14e83e3a0b4ffc9bca14
Neco-arch <111620463+Neco-arch@users.noreply.github.com>
2025-05-24 10:27:28 +0700

    Basic node stuff

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Todolist
 A to do list for people who lazy as hell 🦥🦥
+
+Road map:
+1.Create Function to accept input and show in console
+-Project to seperate to do list 
+-title
+-Description
+-dueDate
+-priority
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+class TodolistData {
+    constructor(title,description,duedate,priority) {
+        this.title = title
+        this.description = description
+        this.duedate = duedate
+        this.priority = priority
+    }
+}
