563c3b9fb1cbdbbd13735e2d55d73d0277d88fe2
Neco-arch <111620463+Neco-arch@users.noreply.github.com>
2025-05-27 20:22:18 +0700

    Fix : make All classes have constructor

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
-import { th } from "date-fns/locale";
 import "./styles.css"
 import { parse } from "date-fns";
 
-const All_Project =  [];
-
-class ConvertToArray {
-    constructor(title,description,duedate,priority,TaskStatus) {
-        this.title = title;
-        this.description = description;
-        this.duedate = duedate;
-        this.TaskStatus = TaskStatus
-        this.priority = priority;
-    };
-}
+const AllProject =  [];
 
 class CreateProjectTodolist {
     constructor(ProjectName) {
@@ -20,35 +9,42 @@ class CreateProjectTodolist {
     };
     CreateProject() {
         const NewProject = this.ProjectName;
-        All_Project.push(NewProject)
+        AllProject.push(NewProject)
     };
 }
 
 
 class Todolist_CollectData_SaveData  {
 
-    CollectData_PutData () {
-        const Project_Name = prompt("What project does this task in ");
-        const Task_title = prompt("What is your task title");
-        const Task_description = prompt("What is your task description");
-        const Task_Status = prompt("What is your task stage right now (Todo,Inprogress,Done)")
-        const Task_DueDate = prompt("What is your due date for your task (DD/MM/YYYY)");
-        const ParseDate = parse(Task_DueDate, 'dd/mm/yyyy', new Date());
-        const Task_priority = prompt("What is your task priority (1-3)");
+    constructor (ProjectName,TaskTitle,TaskDescription,TaskStatus,TaskDueDate,Taskpriority) {
+        this.ProjectName = ProjectName;
+        this.TaskTitle = TaskTitle;
+        this.TaskDescription = TaskDescription;
+        this.Task_Status = TaskStatus;
+        this.Task_DueDate = parse(TaskDueDate, 'dd/mm/yyyy', new Date());
+        this.Task_priority = Taskpriority;
 
-        if (Project.find(item => item === Project_Name)) {
+    }
+    CollectData_PutData () {
+
+        if (AllProject.find(item => item === ProjectName)) {
             console.log("Error");
         } else {
-            const newtask = new ConvertToArray(Task_title,Task_description,ParseDate,Task_priority,Task_Status);
-            All_Project.Project_Name.push(newtask);
+            AllProject.ProjectName.push(this);
         };
     };
 };
 
+class EditDetailsInTodolist {
+    // Tomorrow I will do it 
+}
+
+
+
 
 class SaveDataTolocalStorage {
     SaveDataTolocalStorage() {
-        localStorage.setItem('Project_Task',JSON.stringify(All_Project));
+        localStorage.setItem('Project_Task',JSON.stringify(AllProject));
     };
 
     Retrieve_data_from_localStorage () {
@@ -63,15 +59,19 @@ class SaveDataTolocalStorage {
 };
 
 class DeleteTodolistItems {
+
+    constructor(ProjectName,TaskName) {
+        this.ProjectName = ProjectName;
+        this.TaskName = TaskName;
+    }
+
     deleteTask() {
-        const Project_task = prompt("What does your task you want to delete belong to What Project")
-        const deleteTask_User = prompt("What task do you want to delete?");
         for (let i = 0; i < All_Project.length; i++) {
-            if (All_Project[i] == Project_task) {
+            if (AllProject[i] == Project_task) {
                 for (let i = 0 ; i < Project_task.length ; i++) {
                     if (Project_task[i] == deleteTask_User) {
-                        Project_task.splice(i,1)
-                        console.log("Delete Successfully")
+                        Project_task.splice(i,1);
+                        console.log("Delete Successfully");
                     } 
                 }
             } else {
@@ -83,23 +83,34 @@ class DeleteTodolistItems {
 }
 
 class DeleteProject {
+
+    constructor(ProjectName) {
+        this.ProjectName = ProjectName;
+    };
+
+
     DeleteProject() {
-        if (All_Project === 1) {
+        if (AllProject === 1) {
             console.log("You need atleast 1 project in your todolist");
             return
-        } 
-        const Project_UserWanttodelete = prompt("What project do you want to delete");
+        };
         for (let i = 0 ; i < All_Project.length ; i++) {
-            if (All_Project[i] === Project_UserWanttodelete ) {
-                All_Project.splice(i,1)
-            }
-        }
+            if (AllProject[i] === this.ProjectName ) {
+                AllProject.splice(i,1);
+            };
+        };
     };
 }
 
 class ShowallTaskinList {
     ShowAlltask() {
-        console.log(All_Project)
+        console.log(AllProject)
+    }  
+}
+
+class RunInCLI {
+    RunInCommendLine() {
+
     }
 }
 
